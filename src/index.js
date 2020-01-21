6daf382186af1c110d9966d24c0d0447941c9ccf
Livan Jimenez <jimenez.livan14@gmail.com>
2020-01-21 17:27:46 -0500

    more

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 async function main() {
   const newLink = await prisma.createLink({
@@ -22,27 +26,22 @@ main().catch(e => console.error(e));
 // application schema. Each field in the application schema is 
 // represented by a function with the same name in that object.
 const resolvers = {
-  Query: {
-    info: () => `API for Hackernews Clone!`,
-    feed: () => (root, args, context, info) => {
-      return context.prisma.links();
-    },
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description,
-      })
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 // init server
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 });
 
 server.start(() => console.log(`Running server on port 4000..`));
