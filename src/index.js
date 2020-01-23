9d23c4503ad2df9505f713482d1740b525b8d032
Livan Jimenez <jimenez.livan14@gmail.com>
2020-01-22 21:35:15 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
 const Query = require('./resolvers/Query');
-const Mutation = require('./resolvers/Mutation')
-const User = require('./resolvers/User')
-const Link = require('./resolvers/Link')
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
+const Vote = require('./resolvers/Vote');
 
 async function main() {
   const newLink = await prisma.createLink({
@@ -29,7 +31,9 @@ const resolvers = {
   Query,
   Mutation,
   User,
-  Link
+  Link,
+  Subscription,
+  Vote,
 };
 
 // init server
